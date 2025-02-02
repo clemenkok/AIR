77dd31ee29f8d3294f50faea79227a8e167b5817
lex734 <enxinglao@gmail.com>
2025-02-02 00:27:46 +0000

    use runclaude.py to integrate with frontend

--- runclaude.py
+++ runclaude.py
@@ -5,32 +5,33 @@ client = anthropic.Anthropic(  # defaults to os.environ.get("ANTHROPIC_API_KEY")
     api_key="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",
 )
 
+# not needed for inputs that are only abstracts
 def chunk_text(text, chunk_size=500):
     # Chunk the text into pieces that fit within the token limits
     return [text[i:i + chunk_size * 4] for i in range(0, len(text), chunk_size * 4)]
 
 list_of_txt = []
-directory = "newest_hash"
+directory = "concurrency"
 for fname in os.listdir(directory):
     if fname.lower().endswith('.txt'):
         file_path = os.path.join(directory, fname)
         with open(file_path, 'r') as file:
             file_content = file.read()
-            chunks = chunk_text(file_content)
-            list_of_txt.extend(chunks)
+            #chunks = chunk_text(file_content)
+            list_of_txt.extend(file_content)
 
-def chat_with_Claude(prompt, list_of_txt): #change to buttons guys
+system_message = f"Here are the research papers:\n" + "\n".join([f"{i}. {txt}" for i, txt in enumerate(list_of_txt[:len(list_of_txt)])])
+
+def chat_with_Claude(prompt): #change to buttons guys
     if prompt.isdigit():
         idea_number = int(prompt)
         if idea_number <= 0 or idea_number > 5:
             return output_text == "ERROR, invalid idea number"
         else:
-            prompt = f"From the list of research ideas generated just now, generate the python code based on the research idea number {idea_number}"
-            system_message = []
+            prompt = f"From the list of research ideas generated just now, generate the code to execute the experimentation of research idea number {idea_number}"
     else:
-        prompt = """Based on these papers, create a list of 5 new and original possible ideas for cryptographic hashes. Explain their viability and describe the methodology. Also, include references
+        prompt = """Based on these papers, create a list of 5 new and original possible ideas for further research. Explain their rationale and describe the methodology. Also, include references
             to where inspiration was derived from."""
-        system_message = f"Here are the research papers:\n" + "\n".join([f"{i}. {txt}" for i, txt in enumerate(list_of_txt[:len(list_of_txt)])])
 
     response = client.messages.create(
         model="claude-3-5-sonnet-20241022",
@@ -52,7 +53,7 @@ while True:
         output_text = "Exiting conversation"
         break
     
-    response = chat_with_Claude(user_input, list_of_txt)
+    response = chat_with_Claude(user_input)
     output_text = response.content[0].text
     output_path.write_text(output_text)
     print("Done")
